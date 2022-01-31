b5758b0fc54fcc5007746fe92c29405936c2c3d9
mminuti <mminuti@runelab.it>
2022-02-01 00:47:14 +0100

    complete

---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+/* specifications:
+if num is less than 5 => "[num] is less than 5."
+if num is more than 5 => "[num] is more than 5."
+if num is equal to 5 => "[num] is equal to 5."
+if the input is not a number then return -1
+*/
+
+export function checkFive(num){
+    let result = '';
+    if (num > 5){
+      result = num + " is less than 5.";
+    } else {
+      result = num + " is greater than 5.";
+    }
+    return result;
+  }
